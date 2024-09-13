1d79e692444d166c9a1c0c757d20fdbf4990443f
Ikerquintal26 <ikerquintal96q6@gmail.com>
2024-09-12 18:49:54 -0600

    git commit-m "js2"

--- Validarcorreo.js
+++ Validarcorreo.js
@@ -3,7 +3,7 @@ onsubmit = function(e){
     ok = true;
     if(ok && Correo_electronico.value==""){
         ok=false;
-        alert("Escribir correctamente su email");
+        alert("Debe escribir su Email");
         Correo_electronico.focus();
     }
    
